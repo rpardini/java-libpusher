06a9f5da46d6c82e4dd0399ca826b83e96766ccf
Ricardo Pardini <pardinilist@pardini.net>
2013-01-16 15:59:01 -0200

    Up to java 7. Deploy to local repos. Reformat/reorganize. No functional changes. Up version to 1.0.1-RELEASE. Thanks leacox!

--- src/main/java/com/leacox/pusher/PusherRequest.java
+++ src/main/java/com/leacox/pusher/PusherRequest.java
@@ -3,62 +3,64 @@ package com.leacox.pusher;
 /**
  * Original work Copyright 2010 Stephan Scheuermann
  * Modified work Copyright 2012 John Leacox
- * 
+ *
  * Licensed under the MIT license:
  * http://www.opensource.org/licenses/mit-license.php
  */
 
 /**
  * A class for encapsulating the data needed for a pusher trigger.
- * 
  */
 public class PusherRequest {
+// ------------------------------ FIELDS ------------------------------
 
-	private final String eventName;
-	private final String channelName;
-	private final String jsonData;
-	private final String socketId;
+    private final String eventName;
+    private final String channelName;
+    private final String jsonData;
+    private final String socketId;
 
-	/**
-	 * 
-	 * @param eventName
-	 * @param channelName
-	 * @param jsonData
-	 */
-	public PusherRequest(String channelName, String eventName, String jsonData) {
-		this.channelName = channelName;
-		this.eventName = eventName;
-		this.jsonData = jsonData;
-		this.socketId = "";
-	}
+// --------------------------- CONSTRUCTORS ---------------------------
 
-	/**
-	 * 
-	 * @param eventName
-	 * @param channelName
-	 * @param jsonData
-	 * @param socketId
-	 */
-	public PusherRequest(String channelName, String eventName, String jsonData, String socketId) {
-		this.channelName = channelName;
-		this.eventName = eventName;
-		this.jsonData = jsonData;
-		this.socketId = socketId;
-	}
+    /**
+     * @param eventName
+     * @param channelName
+     * @param jsonData
+     */
+    public PusherRequest(String channelName, String eventName, String jsonData) {
+        this.channelName = channelName;
+        this.eventName = eventName;
+        this.jsonData = jsonData;
+        this.socketId = "";
+    }
 
-	public String getEventName() {
-		return eventName;
-	}
+    /**
+     * @param eventName
+     * @param channelName
+     * @param jsonData
+     * @param socketId
+     */
+    public PusherRequest(String channelName, String eventName, String jsonData, String socketId) {
+        this.channelName = channelName;
+        this.eventName = eventName;
+        this.jsonData = jsonData;
+        this.socketId = socketId;
+    }
 
-	public String getChannelName() {
-		return channelName;
-	}
+// --------------------- GETTER / SETTER METHODS ---------------------
 
-	public String getJsonData() {
-		return jsonData;
-	}
+    public String getChannelName() {
+        return channelName;
+    }
 
-	public String getSocketId() {
-		return socketId;
-	}
+    public String getEventName() {
+        return eventName;
+    }
+
+    public String getJsonData() {
+        return jsonData;
+    }
+
+    public String getSocketId() {
+        return socketId;
+    }
 }
